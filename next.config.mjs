693453dce8d57a06cdad14042bc53e20cdc6c9ef
Tobias Köstl <info@besterfotografderwelt.com>
2026-06-17 16:54:16 +0200

    Revert distDir - nicht der Fehler

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   allowedDevOrigins: ["127.0.0.1"],
+  distDir: process.env.NODE_ENV === "development" ? ".next-dev" : ".next",
   reactStrictMode: true
 };
 
